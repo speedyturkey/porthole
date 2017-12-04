f8dfb8986be29821e7f4701e0c5d1247d90971ab
speedyturkey <speedyturkey@gmail.com>
2017-12-04 17:15:51 -0500

    Expose PortholeLogger; allow logfile to be set in constructor

--- porthole/__init__.py
+++ porthole/__init__.py
@@ -1,15 +1,16 @@
 import sys
 try:
     from .app import config
-    from .getting_started import new_config, setup_tables
     from .connections import ConnectionManager
-    from .queries import QueryGenerator, QueryReader, QueryResult
-    from .xlsx import WorkbookBuilder
+    from .getting_started import new_config, setup_tables
+    from .filters import ResultFilter
+    from .logger import PortholeLogger
     from .mailer import Mailer
-    from .workflows import SimpleWorkflow
     from .related_record import RelatedRecord, ChildRecord
     from .reports import BasicReport, GenericReport
-    from .filters import ResultFilter
+    from .queries import QueryGenerator, QueryReader, QueryResult
+    from .workflows import SimpleWorkflow
+    from .xlsx import WorkbookBuilder
 except KeyError:
     print("Unable to import Porthole due to KeyError. Check config/config.ini.")
     print("{}: {}".format(sys.exc_info()[1].__doc__, sys.exc_info()[1]))
