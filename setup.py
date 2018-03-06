88a6c724f805b05878ceb210fd90fe7d75839af5
speedyturkey <speedyturkey@gmail.com>
2018-03-06 14:58:30 -0500

    Bump to v0.5.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='porthole',
-      version='0.4.4',
+      version='0.5.0',
       description='An automated reporting package.',
       author='Billy McMonagle',
       author_email='speedyturkey@gmail.com',
