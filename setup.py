bd1f1ce75b5558bbbe2847ab241e2e0b87c4d479
Billy McMonagle <speedyturkey@gmail.com>
2019-04-22 16:28:27 -0400

    Bump to v0.7.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='porthole',
-      version='0.7.7',
+      version='0.7.8',
       description='An automated reporting package.',
       author='Billy McMonagle',
       author_email='speedyturkey@gmail.com',
