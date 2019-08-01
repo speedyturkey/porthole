94176f10e39302ab393d476884feaf5e19c9ab90
Billy McMonagle <speedyturkey@gmail.com>
2019-08-01 17:06:18 -0400

    Bump to v0.8.1
    Plus setup.py formatting goodness

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 from setuptools import setup
 
-setup(name='porthole',
-      version='0.8',
-      description='An automated reporting package.',
-      author='Billy McMonagle',
-      author_email='speedyturkey@gmail.com',
-      url='https://github.com/speedyturkey/porthole',
-      packages=['porthole'],
-      python_requires='>3.6',
-      install_requires=[
-                        'pymysql',
-                        'psycopg2-binary',
-                        'xlsxwriter',
-                        'SQLAlchemy',
-                        'pytz'
-                    ],
-      extras_require={
+setup(
+    name='porthole',
+    version='0.8.1',
+    description='An automated reporting package.',
+    author='Billy McMonagle',
+    author_email='speedyturkey@gmail.com',
+    url='https://github.com/speedyturkey/porthole',
+    packages=['porthole'],
+    python_requires='>3.6',
+    install_requires=[
+        'openpyxl',
+        'psycopg2-binary',
+        'pymysql',
+        'pytz',
+        'SQLAlchemy',
+        'xlsxwriter',
+    ],
+    extras_require={
         'AWS': ["boto3"]
-      },
-      zip_safe=False)
+    },
+    zip_safe=False
+)
